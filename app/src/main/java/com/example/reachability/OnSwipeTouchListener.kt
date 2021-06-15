35ab24fbd35457466b744b8065380fbc68a94625
Ethan Burkinshaw <Snowworm3000@gmail.com>
2021-06-15 18:24:15 +0100

    Can listen to swipe events to open screenshot area. Now I am trying to implement the screenshot functionality using the accessibility service.

--- app/src/main/java/com/example/reachability/OnSwipeTouchListener.kt
+++ app/src/main/java/com/example/reachability/OnSwipeTouchListener.kt
@@ -1,50 +1,33 @@
 package com.example.reachability
 
-import android.content.Context
+import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 
 
-class OnSwipeTouchListener(c: Context?) : View.OnTouchListener {
-    private val gestureDetector: GestureDetector
-    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
-        return gestureDetector.onTouchEvent(motionEvent)
+open class OnSwipeTouchListener : OnTouchListener {
+    private val gestureDetector = GestureDetector(GestureListener())
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        Log.d(null,"Touch")
+        return gestureDetector.onTouchEvent(event)
     }
 
-    private inner class GestureListener() : SimpleOnGestureListener() {
-        private val SWIPE_THRESHOLD = 100
-        private val SWIPE_VELOCITY_THRESHOLD = 100
+    private inner class GestureListener : SimpleOnGestureListener() {
+        private val SWIPE_THRESHOLD = 20
+        private val SWIPE_VELOCITY_THRESHOLD = 20
 
-        override fun onDown(e: MotionEvent): Boolean {
-            return true
-        }
-
-        override fun onSingleTapUp(e: MotionEvent): Boolean {
-            onClick()
-            return super.onSingleTapUp(e)
-        }
-
-        override fun onDoubleTap(e: MotionEvent): Boolean {
-            onDoubleClick()
-            return super.onDoubleTap(e)
-        }
-
-        override fun onLongPress(e: MotionEvent) {
-            onLongClick()
-            super.onLongPress(e)
-        }
-
-        // Determines the fling velocity and then fires the appropriate swipe event accordingly
         override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-            val result = false
+            Log.d(null,"fling")
+            var result = false
             try {
                 val diffY = e2.y - e1.y
                 val diffX = e2.x - e1.x
                 if (Math.abs(diffX) > Math.abs(diffY)) {
                     if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffX > 0) {
+                        result = if (diffX > 0) {
                             onSwipeRight()
                         } else {
                             onSwipeLeft()
@@ -52,10 +35,10 @@ class OnSwipeTouchListener(c: Context?) : View.OnTouchListener {
                     }
                 } else {
                     if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffY > 0) {
-                            onSwipeDown()
+                        result = if (diffY > 0) {
+                            onSwipeBottom()
                         } else {
-                            onSwipeUp()
+                            onSwipeTop()
                         }
                     }
                 }
@@ -65,17 +48,22 @@ class OnSwipeTouchListener(c: Context?) : View.OnTouchListener {
             return result
         }
 
+
     }
 
-    fun onSwipeRight() {}
-    fun onSwipeLeft() {}
-    fun onSwipeUp() {}
-    fun onSwipeDown() {}
-    fun onClick() {}
-    fun onDoubleClick() {}
-    fun onLongClick() {}
+    open fun onSwipeRight(): Boolean {
+        return false
+    }
 
-    init {
-        gestureDetector = GestureDetector(c, GestureListener())
+    open fun onSwipeLeft(): Boolean {
+        return false
+    }
+
+    open fun onSwipeTop(): Boolean {
+        return false
+    }
+
+    open fun onSwipeBottom(): Boolean {
+        return false
     }
 }
